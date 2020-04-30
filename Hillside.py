550807442a178ddb0adc048de9d08b089ab0080f
Bazzibabbab <61910223+Bazzibabbab@users.noreply.github.com>
2020-04-30 17:24:14 +0100

    IO

--- Hillside.py
+++ Hillside.py
@@ -12,7 +12,6 @@ import operator
 import matplotlib
 import matplotlib.pyplot
 import matplotlib.animation 
-import agentframework
 import os
 import pathlib
 import csv
@@ -22,25 +21,16 @@ import bs4
 
 # Variables required
 
+
+# Operating system info
+sys = os.name
+workdir = 'C:\\MSc\\Module3\\Python Code\\src\\unpacked\\adm\\Assignment2'
+
 # Main
 
-# Read data from webpage
-
-r = requests.get('https://www.geog.leeds.ac.uk/courses/computing/study/core-python-odl2/assessment2/snow.slope')
-content = r.text
-soup = bs4.BeautifulSoup(content, 'html.parser')
-td_ys = soup.find_all(attrs={"class" : "y"})
-td_xs = soup.find_all(attrs={"class" : "x"})
-
 # Read in data file into Lists
-'''
-f = open('./in.txt', newline='')    # Open file
-reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)    # Read file in with CSV library
-for row in reader:    # A list of rows
-    rowlist = []    # Create blank list
-    for value in row:    # A list of value
-        rowlist.append(value)   # Append each value read to new list
-    environment.append(rowlist)     # Append list to new list
-f.close()    # Don't close until you are done with the reader;
-# data is read on request.
-'''
+
+File = open(os.path.join(workdir, 'snow.slope'))    # Open file
+for Line in File.readlines():
+    print(Line)
+inFile.close()    # close files
