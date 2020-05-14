fb88338225093453003bf9e96cb87e076069b5b7
Bazzibabbab <61910223+Bazzibabbab@users.noreply.github.com>
2020-05-14 19:03:03 +0100

    Added extra comments
    Github desktop issues

--- Hillside.py
+++ Hillside.py
@@ -9,7 +9,9 @@ The resulting DEM and Slope images are displayed through a Tk window
 
 """
 
+#
 # Libraries required
+#
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -17,7 +19,9 @@ import os
 import math
 import tkinter as tk
 
+#
 # Variables required
+#
 
 dem = []        # Blank list to receive DEM into
 demslope=[]     # Blank list to copy DEM slope data into
@@ -27,19 +31,21 @@ cell_count = 0  # Initialise variable to hold count of cells in DEM
 r = 0           # Possibly replace with row
 c = 0           # Possibly replace with column
 matplotlib.use('TkAgg') # Change the backend of matplotlib
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
+fig = plt.figure(figsize=(7, 7))
 
 # Operating system info
 # Look at OS and setting directory delimiter so code works on multiple platforms
-# sys = os.name 
+
 workdir = 'C:\\MSc\\Module3\\Python Code\\src\\unpacked\\adm\\Assignment2'
 infile = os.path.join(workdir, 'snow.slope')
 outfile = os.path.join(workdir, 'slope.txt')
 
-
+#
 # Functions used in code
+#
 
-# Calculates the Slope Rate and Slope Degrees of DEM cell
+# Calculates the Slope Rate and Slope Degrees of a cell using
+# slope_degrees = ATAN ( √ ([dz/dx]2 + [dz/dy]2) ) * 180/pi
 
 def calculate(a, b, c, d, ctr, f, g, h, i):
     deltax = ((c + 2*f + i) - (a + 2*d + g)) / 8    # Calculata deltaX
@@ -48,18 +54,31 @@ def calculate(a, b, c, d, ctr, f, g, h, i):
     slope_deg = math.atan(slope_rate) * (180/math.pi)   # Calculate slope degree from Slope rate
     return(slope_deg)   # Return value
 
-# Button functions
-
+#
+# Button calls for Tk interface
+#
+    
+# Creates a button that will display an DEM image of the input file
+    
 def disp_dem():
     image = plt.imshow(dem)
-    canvas.show()
+    canvas.draw()
+
+# Creates a button that will dsiplay the resulting slope data as an image
 
 def disp_slope():
     image = plt.imshow(demslope)
-    canvas.show()
-    
-# Main
+    canvas.draw()
 
+#
+# Main
+#
+# This is the main body of the program which will define the working path and input file.
+# The file is read into an array using lists. This is then used to feed into the "calculate" function
+# The resulting data is feedback into a new list array to produce a slope file.
+# A Tk interface is used to display either of the images.    
+#
+    
 # Check file exists and read in data file into Lists
 
 if os.path.isfile(infile):
@@ -99,6 +118,7 @@ for line in dem:    # for each line in the input file
     Output.write("%s\n" % rowlist)   # Write the row contents to the output file
     r += 1  # increment the row count
     c = 0   # reset the column count
+
 Output.close()  # close the output file after all data has been added
 
 # Display the DEM
@@ -115,5 +135,5 @@ model_menu.add_command(label="Display DEM", command=disp_dem) # Creates the opti
 model_menu.add_command(label="Display Slope", command=disp_slope) # Creates the option to display the Slope info
 model_menu.add_command(label="Exit",command=root.destroy)   # Creates the option to exit the program
 
-tk.mainloop()
+tk.mainloop()   # wait for user interaction in tk
 
